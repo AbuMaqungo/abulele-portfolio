788a202e99d65e505285c3bfb67567c131ace835
Abulele Maqungo <abu.maqungo@gmail.com>
2025-08-02 04:31:55 +0200

    My portfolio ready for GitHub

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-8">
+      <Head>
+        <title>Abulele Maqungo</title>
+      </Head>
+      <h1 className="text-4xl font-bold text-blue-800">
+        Abulele Maqungo
+      </h1>
+      <p className="text-xl mt-2">
+        Full-Stack Developer | Java, .NET, React
+      </p>
+      
+      
+<div className="mt-6">
+  <Image 
+  src="/abulele.jpg"  // Must match filename exactly
+  alt="Abulele"
+  width={200}         // Required
+  height={200}        // Required
+  className="rounded-full"
+/>
+</div>
+    </div>
+  )
+}
